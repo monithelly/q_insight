3f7eaeee4a55df89f15a6de7ded35c90c5c89698
Monithelly <monithellyfsimoesl@gmail.com>
2026-03-31 20:12:55 -0300

    adicionando dados de exemplo

--- pages/1_Cadastro.py
+++ pages/1_Cadastro.py
@@ -1,40 +1,19 @@
-import streamlit as st
-from database import inserir_ocorrencia, listar_ocorrencias
 import pandas as pd
+from database import inserir_ocorrencia
 
-st.title("📝 Cadastro de Ocorrências")
+if st.button("📥 Carregar dados de exemplo"):
+    df_exemplo = pd.read_csv("dados_exemplo.csv")
 
-with st.form("form_ocorrencia"):
-    data = st.date_input("Data da ocorrência")
-    setor = st.selectbox("Setor", ["Operações", "TI", "Financeiro", "Logística"])
-    responsavel = st.text_input("Responsável")
-    tipo = st.selectbox("Tipo de erro", ["Erro sistema", "Atraso", "Falha processo"])
-    gravidade = st.selectbox("Gravidade", ["Baixa", "Média", "Alta"])
-    status = st.selectbox("Status", ["Aberto", "Resolvido"])
-    tempo = st.number_input("Tempo de resolução (h)", min_value=0)
-    descricao = st.text_area("Descrição")
-
-    botao = st.form_submit_button("Salvar")
-
-    if botao:
+    for _, row in df_exemplo.iterrows():
         inserir_ocorrencia(
-            str(data), setor, responsavel, tipo,
-            gravidade, status, int(tempo), descricao
+            row["data_ocorrencia"],
+            row["setor"],
+            row["responsavel"],
+            row["tipo_erro"],
+            row["gravidade"],
+            row["status"],
+            row["tempo_resolucao"],
+            row["descricao"]
         )
-        st.success("Ocorrência salva!")
 
-st.divider()
-
-st.subheader("📋 Ocorrências cadastradas")
-
-dados = listar_ocorrencias()
-
-if dados:
-    df = pd.DataFrame(dados, columns=[
-        "ID", "Data", "Setor", "Responsável",
-        "Tipo", "Gravidade", "Status",
-        "Tempo", "Descrição"
-    ])
-    st.dataframe(df)
-else:
-    st.info("Nenhuma ocorrência ainda")
+    st.success("Dados de exemplo carregados com sucesso!")
