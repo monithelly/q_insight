d58cf534866e59029d79fc9b12eac546d97d55c9
Monithelly <monithellyfsimoesl@gmail.com>
2026-03-31 20:17:47 -0300

    atualização dados e pagina

--- pages/1_Cadastro.py
+++ pages/1_Cadastro.py
@@ -1,5 +1,8 @@
+import streamlit as st
 import pandas as pd
-from database import inserir_ocorrencia
+from database import inserir_ocorrencia, listar_ocorrencias
+
+st.title("📝 Cadastro de Ocorrências")
 
 if st.button("📥 Carregar dados de exemplo"):
     df_exemplo = pd.read_csv("dados_exemplo.csv")
@@ -12,8 +15,48 @@ if st.button("📥 Carregar dados de exemplo"):
             row["tipo_erro"],
             row["gravidade"],
             row["status"],
-            row["tempo_resolucao"],
+            int(row["tempo_resolucao"]),
             row["descricao"]
         )
 
     st.success("Dados de exemplo carregados com sucesso!")
+
+with st.form("form_ocorrencia"):
+    data = st.date_input("Data da ocorrência")
+    setor = st.selectbox("Setor", ["Operações", "TI", "Financeiro", "Logística", "Atendimento"])
+    responsavel = st.text_input("Responsável")
+    tipo = st.selectbox("Tipo de erro", ["Erro sistema", "Atraso", "Falha processo", "Erro humano", "Retrabalho"])
+    gravidade = st.selectbox("Gravidade", ["Baixa", "Média", "Alta"])
+    status = st.selectbox("Status", ["Aberto", "Em andamento", "Resolvido"])
+    tempo = st.number_input("Tempo de resolução (h)", min_value=0)
+    descricao = st.text_area("Descrição")
+
+    botao = st.form_submit_button("Salvar")
+
+    if botao:
+        inserir_ocorrencia(
+            str(data),
+            setor,
+            responsavel,
+            tipo,
+            gravidade,
+            status,
+            int(tempo),
+            descricao
+        )
+        st.success("Ocorrência salva!")
+
+st.divider()
+st.subheader("📋 Ocorrências cadastradas")
+
+dados = listar_ocorrencias()
+
+if dados:
+    df = pd.DataFrame(dados, columns=[
+        "ID", "Data", "Setor", "Responsável",
+        "Tipo", "Gravidade", "Status",
+        "Tempo", "Descrição"
+    ])
+    st.dataframe(df, width="stretch")
+else:
+    st.info("Nenhuma ocorrência ainda.")
